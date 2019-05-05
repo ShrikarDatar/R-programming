197874c6c4dfedfe8320b01235652f5b1778427e
ShrikarDatar <47650216+ShrikarDatar@users.noreply.github.com>
2019-05-05 14:40:37 +0530

    Create PositiveNegativeOddEven.R

---
+++ PositiveNegativeOddEven.R
@@ -0,0 +1,23 @@
+#Display a number positive or negative
+num=1
+if(num==0)
+{
+  print('zero')
+}else if(num>0)
+{
+  print("Positive")
+}else
+{
+  print("Negative")
+}
+
+#Odd Even
+num=as.integer(readline(prompt = "Enter a number"))
+if(num%%2=0)
+{
+  print('Even')
+  
+}else
+{
+  print('Odd')
+}
