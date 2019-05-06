bac3be41587eb28a9e37b29ff69b75a7089cb922
ShrikarDatar <47650216+ShrikarDatar@users.noreply.github.com>
2019-05-06 14:54:30 +0530

    Update FibonacciRec.R

--- FibonacciRec.R
+++ FibonacciRec.R
@@ -12,3 +12,22 @@ fibonacci = function(n)
   return(fibonacci(n-1)+fibonacci(n-2))
 }
 fibonacci(15)
+#without recursion
+
+fibonacci<-function()
+{
+  f1=0
+  f2=1
+  f3=0
+  num=as.integer(readline(prompt = "Enter a number"))
+  print(f1)
+  print(f2)
+  while(num>0) {
+    f3=f1+f2
+    print(f3)
+    f1=f2
+    f2=f3
+    num=num-1
+  }
+}
+fibonacci()
